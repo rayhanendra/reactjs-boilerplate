ed690d514eaa30cf0879252245cedf86871e940d
Rayhanendra <mrayhann@gmail.com>
2022-06-13 21:54:12 +0700

    fixes

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,38 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
-  entry: "./index.js",
-  mode: "development",
+  entry: './index.js',
+  mode: 'development',
   output: {
-    path: path.resolve(__dirname, "./dist"),
-    filename: "index_bundle.js",
+    path: path.resolve(__dirname, './dist'),
+    filename: 'index_bundle.js'
   },
-  target: "web",
+  target: 'web',
   devServer: {
-    port: "5000",
+    port: '5000',
     static: {
-      directory: path.join(__dirname, "public"),
+      directory: path.join(__dirname, 'public')
     },
     open: true,
     hot: true,
-    liveReload: true,
+    liveReload: true
   },
   resolve: {
-    extensions: [".js", ".jsx", ".json"],
+    extensions: ['.js', '.jsx', '.json']
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: "babel-loader",
-      },
-    ],
+        use: 'babel-loader'
+      }
+    ]
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, "public", "index.html"),
-    }),
-  ],
+      template: path.join(__dirname, 'public', 'index.html')
+    })
+  ]
 };
